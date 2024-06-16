4ed5f06cb28f80c7e1e423a298343e96ac6d9f3a
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-06-15 22:25:39 -0500

    Creando categroias

--- app/src/main/java/com/wo/burgerblend/activity/MainActivity.kt
+++ app/src/main/java/com/wo/burgerblend/activity/MainActivity.kt
@@ -5,9 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.wo.burgerblend.R
+import com.wo.burgerblend.domain.Categoria
 
 class MainActivity : AppCompatActivity() {
+
+    var adapter: RecyclerView.Adapter<*>? = null
+    var recyclerViewCategorias: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +24,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerViewCategoria()
+    }
+
+    private fun recyclerViewCategoria() {
+        val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewCategorias = findViewById(R.id.recyclerView_categoria)
+        recyclerViewCategorias!!.layoutManager = linearLayoutManager
+
+        val categorias: ArrayList<Categoria> = ArrayList()
+        categorias.add(Categoria(1, "Pizzas", "cat_1"))
+        categorias.add(Categoria(2, "Hamburguesas", "cat_2"))
+        categorias.add(Categoria(3, "Hot Dogs", "cat_3"))
+        categorias.add(Categoria(4, "Bebidas", "cat_4"))
+        categorias.add(Categoria(5, "Donas", "cat_5"))
+
+        /*adapter = CategoriaAdapter(this)
+        recyclerViewCategorias!!.adapter = adapter*/
     }
 }
