30577fc3d811a49d709875f0fa79928b1f682c55
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-06-15 19:17:46 -0500

    Main activity cabecera

--- app/src/main/java/com/wo/burgerblend/activity/IntroActivity.kt
+++ app/src/main/java/com/wo/burgerblend/activity/IntroActivity.kt
@@ -1,8 +1,11 @@
 package com.wo.burgerblend.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.wo.burgerblend.R
@@ -16,6 +19,13 @@ class IntroActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnEmpezar: ConstraintLayout = findViewById(R.id.button_empezar)
+
+        btnEmpezar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
