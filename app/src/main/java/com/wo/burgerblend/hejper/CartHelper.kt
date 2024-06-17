2dcc95ea7c305fcd19b05d2b101eeeed0fcc752a
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-06-16 19:13:56 -0500

    Actualizando

--- app/src/main/java/com/wo/burgerblend/hejper/CartHelper.kt
+++ app/src/main/java/com/wo/burgerblend/hejper/CartHelper.kt
@@ -1,34 +1,4 @@
 package com.wo.burgerblend.hejper
 
-import android.content.Context
-import com.wo.burgerblend.domain.Food
-
-class CartHelper(val context: Context, val dataStorageHelper: DataStorageHelper = DataStorageHelper(context)) {
-    fun addToCart(food: Food) {
-        //dataStorageHelper.addToCart(food)
-        //val foods: ArrayList<Food> = getCart()
-            val exists: Boolean = true
-    }
-/*
-    fun removeFromCart(food: Food) {
-        dataStorageHelper.removeFromCart(food)
-    }
-    fun getCart(): ArrayList<Food> {
-        return dataStorageHelper.getListObject("cart")
-    }
-    fun clearCart() {
-        return dataStorageHelper.clearCart()
-    }
-    fun getCartSize(): Int {
-        return dataStorageHelper.getCartSize()
-    }
-    fun getCartTotal(): Double {
-        return dataStorageHelper.getCartTotal()
-    }
-    fun
-*/
-
-
-
-
+class CartHelper() {
 }
